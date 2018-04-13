0c701e86844809ec1ab6c5f81dc1ffc0634bb467
Gandus10 <laurent.gander7@gmail.com>
2018-04-13 14:10:56 +0200

    Project basis

---
+++ ArcQuiz/ArcQuiz/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ArcQuiz
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+        }
+
+        async void OpenEngineerPage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EngineerPage());
+        }
+
+        async void OpenNursePage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NursePage());
+        }
+    }
+}
